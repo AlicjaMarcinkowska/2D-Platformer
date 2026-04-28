be7780aa485c84d5dcaedb7650540b93a6a067ac
AlicjaMarcinkowska <dsw61084@student.dsw.edu.pl>
2026-04-28 10:35:16 +0200

    5 poziomów

---
+++ Assets/SceneOpener.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TryAgain : MonoBehaviour
+{
+    public string sceneName;
+    public void OpenScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
